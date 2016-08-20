9428709ff0c221cf9aa43639f363a46eb3a2f44f
James Seward <james@jamesoff.net>
2016-08-20 14:30:57 +0100

    Rename test files

--- tests/unittest.sh
+++ tests/unittest.sh
@@ -4,8 +4,10 @@ set -x
 
 if [[ $TRAVIS_PYTHON_VERSION = 2.6* ]]; then
 	pip install unittest2
-	mv tests/tests.py .
-	sed -i -e 's/import unittest/import unittest2 as unittest/' tests.py
+	for f in tests/test_*.py; do
+		mv "tests/$f" .
+		sed -i -e 's/import unittest/import unittest2 as unittest/' "$f"
+	done
 	unit2 discover
 else
 	python -m unittest discover -s tests
