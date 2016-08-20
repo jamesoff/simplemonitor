91e9a7e3cfb33498242b63ca233a9714e6c04f35
James Seward <james@jamesoff.net>
2016-08-20 14:33:40 +0100

    Fix paths

--- tests/unittest.sh
+++ tests/unittest.sh
@@ -4,9 +4,10 @@ set -x
 
 if [[ $TRAVIS_PYTHON_VERSION = 2.6* ]]; then
 	pip install unittest2
-	for f in tests/test_*.py; do
-		mv "tests/$f" .
-		sed -i -e 's/import unittest/import unittest2 as unittest/' "$f"
+	cd tests
+	for f in test_*.py; do
+		mv "$f" ..
+		sed -i -e 's/import unittest/import unittest2 as unittest/' "../$f"
 	done
 	unit2 discover
 else
