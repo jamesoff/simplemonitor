f0378756d37994d9fc79bd926ec802b7edebe51c
James Seward <james@jamesoff.net>
2016-08-20 14:36:03 +0100

    .

--- tests/unittest.sh
+++ tests/unittest.sh
@@ -4,11 +4,13 @@ set -x
 
 if [[ $TRAVIS_PYTHON_VERSION = 2.6* ]]; then
 	pip install unittest2
-	cd tests
-	for f in test_*.py; do
-		mv "$f" ..
-		sed -i -e 's/import unittest/import unittest2 as unittest/' "../$f"
-	done
+	(
+		cd tests || exit 1
+		for f in test_*.py; do
+			mv "$f" ..
+			sed -i -e 's/import unittest/import unittest2 as unittest/' "../$f"
+		done
+	)
 	unit2 discover
 else
 	python -m unittest discover -s tests
