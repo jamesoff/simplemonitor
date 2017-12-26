9948650dbe7d42a3f0935b31aa7ca8d4600f8348
James Seward <james@jamesoff.net>
2017-12-26 14:35:51 +0000

    Make envconfig work better in Python3
    And hopefully Python2 too

--- envconfig.py
+++ envconfig.py
@@ -6,6 +6,7 @@ if sys.version_info.major == 2:
     from ConfigParser import ConfigParser
 else:
     from configparser import ConfigParser
+    from configparser import BasicInterpolation
 
 
 class EnvironmentAwareConfigParser(ConfigParser):
@@ -14,7 +15,14 @@ class EnvironmentAwareConfigParser(ConfigParser):
 
     r = re.compile('%env:([a-zA-Z0-9_]+)%')
 
+    def __init__(self, *args, **kwargs):
+        """Init with our specific interpolation class. Safe on Python 2 as it ignores kwargs it doesn't know."""
+        interpolation = EnvironmentAwareInterpolation()
+        kwargs['interpolation'] = interpolation
+        super().__init__(*args, **kwargs)
+
     def read(self, filenames):
+        """Load a config file and do environment variable interpolation on the section names."""
         result = ConfigParser.read(self, filenames)
         for section in self.sections():
             original_section = section
@@ -23,6 +31,9 @@ class EnvironmentAwareConfigParser(ConfigParser):
                 env_key = matches.group(1)
                 if env_key in os.environ:
                     section = section.replace(matches.group(0), os.environ[env_key])
+                else:
+                    raise ValueError('Cannot find {} in environment for config interpolation'.format(env_key))
+
                 matches = self.r.search(section)
             if section != original_section:
                 self.add_section(section)
@@ -32,11 +43,34 @@ class EnvironmentAwareConfigParser(ConfigParser):
         return result
 
     def get(self, *args, **kwargs):
-        result = ConfigParser.get(self, *args, **kwargs)
+        if sys.version_info.major > 2:
+            return super().get(*args, **kwargs)
+        result = super().get(*args, **kwargs)
         matches = self.r.search(result)
         while matches:
             env_key = matches.group(1)
             if env_key in os.environ:
                 result = result.replace(matches.group(0), os.environ[env_key])
-            matches = self.r.search(result)
+            matches = self.r.match(result)
         return result
+
+if sys.version_info.major > 2:
+    class EnvironmentAwareInterpolation(BasicInterpolation):
+        r = re.compile('%env:([a-zA-Z0-9_]+)%')
+
+        def before_get(self, parser, section, option, value, defaults):
+            parser.get(section, option, raw=True, fallback=value)
+            matches = self.r.search(value)
+            old_value = value
+            while matches:
+                env_key = matches.group(1)
+                if env_key in os.environ:
+                    value = value.replace(matches.group(0), os.environ[env_key])
+                else:
+                    raise ValueError('Cannot find {} in environment for config interpolation'.format(env_key))
+                matches = self.r.search(value)
+                if value == old_value:
+                    break
+                old_value = value
+            return value
+
