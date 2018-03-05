fb4f56bd31da9bab549a3ce4e2a9bdd7431cf370
James Seward <james@jamesoff.net>
2018-03-05 22:50:20 +0000

    Repair flake8 issues in pushbullet

--- Alerters/pushbullet.py
+++ Alerters/pushbullet.py
@@ -12,7 +12,7 @@ class PushbulletAlerter(Alerter):
         Alerter.__init__(self, config_options)
         try:
             pushbullet_token = config_options["token"]
-        except:
+        except Exception:
             raise RuntimeError("Required configuration fields missing")
 
         if pushbullet_token == "":
@@ -73,27 +73,24 @@ class PushbulletAlerter(Alerter):
         elif type == "success":
             subject = "[%s] Monitor %s succeeded" % (self.hostname, name)
             body = "Monitor %s%s is back up.\nOriginally failed at: %s\nDowntime: %d+%02d:%02d:%02d\nDescription: %s" % (
-            name, host, self.format_datetime(monitor.first_failure_time()), days, hours, minutes, seconds,
-            monitor.describe())
+                name, host, self.format_datetime(monitor.first_failure_time()), days, hours, minutes, seconds,
+                monitor.describe())
 
         elif type == "catchup":
             subject = "[%s] Monitor %s failed earlier!" % (self.from_addr, self.to_addr, self.hostname, name)
             body = "Monitor %s%s failed earlier while this alerter was out of hours.\nFailed at: %s\nVirtual failure count: %d\nAdditional info: %s\nDescription: %s" % (
-            name, host, self.format_datetime(monitor.first_failure_time()), monitor.virtual_fail_count(),
-            monitor.get_result(), monitor.describe())
+                name, host, self.format_datetime(monitor.first_failure_time()), monitor.virtual_fail_count(),
+                monitor.get_result(), monitor.describe())
 
         else:
-            print
-            "Unknown alert type %s" % type
+            print("Unknown alert type %s" % type)
             return
 
         if not self.dry_run:
             try:
                 self.send_pushbullet_notification(subject, body)
-            except Exception, e:
-                print
-                "Couldn't send push notification: %s", e
+            except Exception as e:
+                print("Couldn't send push notification: %s", e)
                 self.available = False
         else:
-            print
-            "dry_run: would send push notification: %s" % body
+            print("dry_run: would send push notification: %s" % body)
