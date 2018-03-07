7c334f267d7258198680d84c288226cc98cac300
James Seward <james@jamesoff.net>
2018-03-07 21:09:58 +0000

    Test network loggers

---
+++ tests/test-network.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -ex
+
+rm -f network.log
+
+# start the master instance
+coverage run --append monitor.py -f tests/network/master/monitor.ini -d &
+sleep 5
+
+# run the client instance
+coverage run --append monitor.py -f tests/network/client/monitor.ini -1 -d &
+
+# let them run
+sleep 25
+
+# shut down the master instance
+kill %1
+kill %2
+
+# make sure the client reached the master 
+grep test2 network.log
