7e5cb3f172201e7a5b113ffb2d60d5c6876f46e8
James Seward <james@jamesoff.net>
2018-03-09 23:39:21 +0000

    Run tests without killing them
    Hopefully this will keep all the coverage data

--- tests/test-network.sh
+++ tests/test-network.sh
@@ -5,23 +5,18 @@ set -ex
 rm -f network.log
 
 # start the master instance
-coverage run -p monitor.py -f tests/network/master/monitor.ini -d &
-sleep 5
+COVERAGE_FILE=.coverage.1 coverage run --debug=dataio monitor.py -f tests/network/master/monitor.ini -d --loops=2 &
+sleep 1
 
 # run the client instance
-coverage run -p monitor.py -f tests/network/client/monitor.ini -1 -d &
+COVERAGE_FILE=.coverage.2 coverage run --debug=dataio monitor.py -f tests/network/client/monitor.ini -1 -d
 
 # let them run
-sleep 25
-
-# shut down the master instance
-kill %1
-kill %2
+sleep 15
 
 # make sure the client reached the master 
 grep test2 network.log
 
-# make sure both instances have exited before we combine logs
 wait
 
 coverage combine --append
