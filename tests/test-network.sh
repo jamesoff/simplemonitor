1e720f5bd1663b3a86c742db42abacb1a03aa8a4
James Seward <james@jamesoff.net>
2018-03-09 23:01:03 +0000

    Try again to make coverage combine

--- tests/test-network.sh
+++ tests/test-network.sh
@@ -5,11 +5,11 @@ set -ex
 rm -f network.log
 
 # start the master instance
-coverage run --append monitor.py -f tests/network/master/monitor.ini -d &
+coverage run -p monitor.py -f tests/network/master/monitor.ini -d &
 sleep 5
 
 # run the client instance
-COVERAGE_FILE=.coverage.2 coverage run --append monitor.py -f tests/network/client/monitor.ini -1 -d &
+coverage run -p monitor.py -f tests/network/client/monitor.ini -1 -d &
 
 # let them run
 sleep 25
@@ -20,3 +20,5 @@ kill %2
 
 # make sure the client reached the master 
 grep test2 network.log
+
+coverage combine --append
