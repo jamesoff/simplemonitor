0f2cd1c8b44a8ace436c04800c93f626d29e1843
James Seward <james@jamesoff.net>
2018-03-11 18:10:44 +0000

    Add more tests for Alerter

--- tests/test_alerter.py
+++ tests/test_alerter.py
@@ -27,3 +27,61 @@ class TestAlerter(unittest.TestCase):
         self.assertEqual(a.time_info, [
             datetime.time(10, 00), datetime.time(11, 00)
         ])
+
+    def test_times_not(self):
+        config_options = {
+            'times_type': 'not',
+            'time_lower': '10:00',
+            'time_upper': '11:00'
+        }
+        a = Alerters.alerter.Alerter(config_options)
+        self.assertEqual(a.times_type, 'not')
+        self.assertEqual(a.time_info, [
+            datetime.time(10, 00), datetime.time(11, 00)
+        ])
+
+    def test_times_broken(self):
+        config_options = {
+            'times_type': 'fake',
+            'time_lower': '10:00',
+            'time_upper': '11:00'
+        }
+        with self.assertRaises(RuntimeError):
+            Alerters.alerter.Alerter(config_options)
+
+    def test_days(self):
+        config_options = {
+            'days': '0,1,4'
+        }
+        a = Alerters.alerter.Alerter(config_options)
+        self.assertEqual(a.days, [0, 1, 4])
+
+    def test_delay(self):
+        config_options = {
+            'delay': '1'
+        }
+        a = Alerters.alerter.Alerter(config_options)
+        self.assertEqual(a.delay_notification, True)
+
+    def test_dryrun(self):
+        config_options = {
+            'dry_run': '1'
+        }
+        a = Alerters.alerter.Alerter(config_options)
+        self.assertEqual(a.dry_run, True)
+
+    def test_oohrecovery(self):
+        config_otions = {
+            'ooh_recovery': '1'
+        }
+        a = Alerters.alerter.Alerter(config_otions)
+        self.assertEqual(a.ooh_recovery, True)
+
+    def test_dependencies(self):
+        config_options = {
+            'depend': 'a,b,c'
+        }
+        a = Alerters.alerter.Alerter(config_options)
+        self.assertEqual(a.dependencies, ['a', 'b', 'c'], 'Alerter did not store dependencies')
+        self.assertEqual(a.check_dependencies(['d', 'e']), None, 'Alerter thinks a dependency failed')
+        self.assertEqual(a.check_dependencies(['a']), False, 'Alerter did not notice a dependency failed')
