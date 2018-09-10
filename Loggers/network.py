332e6cca2cb4c648ee569de05baddab7088c1c7b
James Seward <james@jamesoff.net>
2018-09-10 19:27:08 +0100

    Display local and remote digests as hexstring not raw bytes
    This will improve output in logging.

--- Loggers/network.py
+++ Loggers/network.py
@@ -126,7 +126,7 @@ class Listener(Thread):
                     my_digest = mac.digest()
                 except IndexError:
                     raise ValueError('Did not receive any or enough data from %s', addr[0])
-                self.logger.debug("Computed my digest to be %s; remote is %s", my_digest, their_digest)
+                self.logger.debug("Computed my digest to be %s; remote is %s", my_digest.hex(), their_digest.hex())
                 if not hmac.compare_digest(their_digest, my_digest):
                     raise Exception("Mismatched MAC for network logging data from %s\nMismatched key? Old version of SimpleMonitor?\n" % addr[0])
                 result = pickle.loads(pickled)
