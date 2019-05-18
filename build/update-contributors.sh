58cc6711bb6b55c9d020698d08f4afe0f3c3411f
James Seward <james@jamesoff.net>
2019-05-18 23:06:59 +0100

    Add CONTRIBUTORS and build script for it

---
+++ build/update-contributors.sh
@@ -0,0 +1,7 @@
+#1/usr/bin/env bash
+
+contributors=CONTRIBUTORS
+[[ -f "$contributors".tmp ]] && rm -f "$contributors".tmp
+git shortlog -s | awk '{print $2}' > "$contributors".tmp
+sort -u CONTRIBUTORS.tmp > CONTRIBUTORS
+[[ -f "$contributors".tmp ]] && rm -f "$contributors".tmp
