63f056633ff3c00522e58500f09af8e5a306d53a
James Seward <james@jamesoff.net>
2019-05-19 16:19:28 +0100

    Remove Py2 handling from envconfig

--- envconfig.py
+++ envconfig.py
@@ -4,38 +4,8 @@ import functools
 
 import sys
 
-if sys.version_info[0] == 2:
-    from ConfigParser import ConfigParser, NoSectionError, NoOptionError
-else:
-    from configparser import ConfigParser, NoSectionError, NoOptionError
-    from configparser import BasicInterpolation
-
-
-# Unique object, which replaces a default config value
-_NO_FALLBACK = object()
-
-
-if sys.version_info[0] == 2:
-
-    def with_fallback(f):
-        @functools.wraps(f)
-        def newf(*args, **kwargs):
-            fallback = kwargs.pop("fallback", _NO_FALLBACK)
-            try:
-                return f(*args, **kwargs)
-            except (NoSectionError, NoOptionError):
-                if fallback is _NO_FALLBACK:
-                    raise
-                else:
-                    return fallback
-
-        return newf
-
-
-else:
-
-    def with_fallback(f):
-        return f
+from configparser import ConfigParser, NoSectionError, NoOptionError
+from configparser import BasicInterpolation
 
 
 class EnvironmentAwareConfigParser(ConfigParser):
@@ -46,12 +16,8 @@ class EnvironmentAwareConfigParser(ConfigParser):
 
     def __init__(self, *args, **kwargs):
         """Init with our specific interpolation class (for Python 3)"""
-        try:
-            interpolation = EnvironmentAwareInterpolation()
-            kwargs["interpolation"] = interpolation
-        except Exception:
-            # Python 2
-            pass
+        interpolation = EnvironmentAwareInterpolation()
+        kwargs["interpolation"] = interpolation
         ConfigParser.__init__(self, *args, **kwargs)
 
     def read(self, filenames):
@@ -79,55 +45,26 @@ class EnvironmentAwareConfigParser(ConfigParser):
                 self.remove_section(original_section)
         return result
 
-    @with_fallback
-    def get(self, *args, **kwargs):
-        if sys.version_info[0] > 2:
-            return ConfigParser.get(self, *args, **kwargs)
-        result = ConfigParser.get(self, *args, **kwargs)
-        matches = self.r.search(result)
+
+class EnvironmentAwareInterpolation(BasicInterpolation):
+    r = re.compile("%env:([a-zA-Z0-9_]+)%")
+
+    def before_get(self, parser, section, option, value, defaults):
+        parser.get(section, option, raw=True, fallback=value)
+        matches = self.r.search(value)
+        old_value = value
         while matches:
             env_key = matches.group(1)
             if env_key in os.environ:
-                result = result.replace(matches.group(0), os.environ[env_key])
-            matches = self.r.match(result)
-        return result
-
-    if sys.version_info[0] == 2:
-
-        @with_fallback
-        def getint(self, *args, **kwargs):
-            return ConfigParser.getint(self, *args, **kwargs)
-
-        @with_fallback
-        def getfloat(self, *args, **kwargs):
-            return ConfigParser.getfloat(self, *args, **kwargs)
-
-        @with_fallback
-        def getboolean(self, *args, **kwargs):
-            return ConfigParser.getboolean(self, *args, **kwargs)
-
-
-if sys.version_info[0] > 2:
-
-    class EnvironmentAwareInterpolation(BasicInterpolation):
-        r = re.compile("%env:([a-zA-Z0-9_]+)%")
-
-        def before_get(self, parser, section, option, value, defaults):
-            parser.get(section, option, raw=True, fallback=value)
-            matches = self.r.search(value)
-            old_value = value
-            while matches:
-                env_key = matches.group(1)
-                if env_key in os.environ:
-                    value = value.replace(matches.group(0), os.environ[env_key])
-                else:
-                    raise ValueError(
-                        "Cannot find {0} in environment for config interpolation".format(
-                            env_key
-                        )
+                value = value.replace(matches.group(0), os.environ[env_key])
+            else:
+                raise ValueError(
+                    "Cannot find {0} in environment for config interpolation".format(
+                        env_key
                     )
-                matches = self.r.search(value)
-                if value == old_value:
-                    break
-                old_value = value
-            return value
+                )
+            matches = self.r.search(value)
+            if value == old_value:
+                break
+            old_value = value
+        return value
