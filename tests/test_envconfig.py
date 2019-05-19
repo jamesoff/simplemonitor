cbdfe6a8e52ea58bbd56920782db967a23f67918
James Seward <james@jamesoff.net>
2019-05-19 16:30:10 +0100

    Add unit tests for config parser

---
+++ tests/test_envconfig.py
@@ -0,0 +1,17 @@
+import unittest
+import envconfig
+import os
+
+os.environ["TEST_VALUE"] = "test1"
+
+class TestEnvConfig(unittest.TestCase):
+
+    def test_EnvConfig(self):
+        config = envconfig.EnvironmentAwareConfigParser()
+        config.read("tests/monitor-env.ini")
+        self.assertEqual(config.get("monitor", "monitors"), "tests/monitors-test1.ini")
+
+    def test_EnvConfigKey(self):
+        config = envconfig.EnvironmentAwareConfigParser()
+        config.read("tests/monitor-env.ini")
+        self.assertEqual(config.get("monitor-test1", "monitors"), "hello")
