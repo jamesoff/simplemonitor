5653882fa41bb9c7d87fdf50c3ac8194dc207e46
James Seward <james@jamesoff.net>
2019-11-23 21:04:04 +0000

    Decode output to bytes in exception case

--- Monitors/host.py
+++ Monitors/host.py
@@ -301,7 +301,7 @@ class MonitorPkgAudit(Monitor):
             try:
                 output = subprocess.check_output([self.path, "audit"]).decode("utf-8")
             except subprocess.CalledProcessError as e:
-                output = e.output
+                output = e.output.decode("utf-8")
             except OSError as e:
                 return self.record_fail(
                     "Failed to run %s audit: {0} {1}".format(self.path, e)
