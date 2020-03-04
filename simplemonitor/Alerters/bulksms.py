61ef9b77668f7a8fae66958c60479d5e88beec3c
James Seward <james@jamesoff.net>
2020-03-04 23:05:09 +0000

    Update bulksms to build_message

--- simplemonitor/Alerters/bulksms.py
+++ simplemonitor/Alerters/bulksms.py
@@ -1,11 +1,11 @@
 # coding=utf-8
 
-from typing import Any, cast
+from typing import cast
 
 import requests
 
-from ..util import format_datetime
-from .alerter import Alerter, AlertType, register
+from ..Monitors.monitor import Monitor
+from .alerter import Alerter, AlertLength, AlertType, register
 
 
 @register
@@ -29,7 +29,6 @@ class BulkSMSAlerter(Alerter):
         )
 
         self.sender = cast(str, self.get_config_option("sender", default="SmplMntr"))
-        assert isinstance(self.sender, str)
         if len(self.sender) > 11:
             self.alerter_logger.warning("truncating SMS sender name to 11 chars")
             self.sender = self.sender[:11]
@@ -38,65 +37,27 @@ class BulkSMSAlerter(Alerter):
 
         self.support_catchup = True
 
-    def send_alert(self, name: str, monitor: Any) -> None:
+    def send_alert(self, name: str, monitor: Monitor) -> None:
         """Send an SMS alert."""
 
         if not monitor.urgent:
             return
 
         alert_type = self.should_alert(monitor)
-        message = ""
-        url = ""
-
-        downtime = monitor.get_downtime()
-        if alert_type == AlertType.NONE:
+        if alert_type not in [AlertType.FAILURE, AlertType.SUCCESS]:
             return
-        elif alert_type == AlertType.CATCHUP:
-            message = "catchup: %s failed on %s at %s (%s)\n%s" % (
-                name,
-                monitor.running_on,
-                format_datetime(monitor.first_failure_time()),
-                downtime,
-                monitor.get_result(),
-            )
-            if len(message) > 160:
-                self.alerter_logger.warning("Truncating SMS message to 160 chars.")
-                message = message[:156] + "..."
-            url = "https://{}/eapi/submission/send_sms/2/2.0".format(self.api_host)
-            params = {
-                "username": self.username,
-                "password": self.password,
-                "message": message,
-                "msisdn": self.target,
-                "sender": self.sender,
-                "repliable": "0",
-            }
-        elif alert_type == AlertType.FAILURE:
-            message = "%s failed on %s at %s (%s)\n%s" % (
-                name,
-                monitor.running_on,
-                format_datetime(monitor.first_failure_time()),
-                downtime,
-                monitor.get_result(),
-            )
-            if len(message) > 160:
-                self.alerter_logger.warning("Truncating SMS message to 160 chars.")
-                message = message[:156] + "..."
-            url = "https://{}/eapi/submission/send_sms/2/2.0".format(self.api_host)
-            params = {
-                "username": self.username,
-                "password": self.password,
-                "message": message,
-                "msisdn": self.target,
-                "sender": self.sender,
-                "repliable": "0",
-            }
-        else:
-            # we don't handle other types of message
-            pass
 
-        if url == "":
-            return
+        message = self.build_message(AlertLength.SMS, alert_type, monitor)
+
+        url = "https://{}/eapi/submission/send_sms/2/2.0".format(self.api_host)
+        params = {
+            "username": self.username,
+            "password": self.password,
+            "message": message,
+            "msisdn": self.target,
+            "sender": self.sender,
+            "repliable": "0",
+        }
 
         if not self._dry_run:
             try:
@@ -111,5 +72,6 @@ class BulkSMSAlerter(Alerter):
                 self.alerter_logger.exception("SMS sending failed")
                 self.available = False
         else:
-            self.alerter_logger.info("dry_run: would send SMS: %s", url)
-        return
+            self.alerter_logger.info(
+                "dry_run: would send SMS: {} with message {}".format(url, message)
+            )
