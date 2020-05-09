87c35ff4864502e6e3175d383a5977b8510e8ac9
James Seward <james@jamesoff.net>
2020-05-09 13:51:15 +0100

    Tidy up imports

--- simplemonitor/Alerters/__init__.py
+++ simplemonitor/Alerters/__init__.py
@@ -1,1 +1,16 @@
-__all__ = ["alerter", "mail", "ses", "bulksms", "pushbullet"]
+"""
+Alerters for SimpleMonitor
+"""
+
+from .bulksms import BulkSMSAlerter
+from .execute import ExecuteAlerter
+from .fortysixelks import FortySixElksAlerter
+from .mail import EMailAlerter
+from .nc import NotificationCenterAlerter
+from .pushbullet import PushbulletAlerter
+from .pushover import PushoverAlerter
+from .ses import SESAlerter
+from .slack import SlackAlerter
+from .sns import SNSAlerter
+from .syslogger import SyslogAlerter
+from .telegram import TelegramAlerter
--- simplemonitor/Monitors/__init__.py
+++ simplemonitor/Monitors/__init__.py
@@ -1,1 +1,32 @@
-__all__ = ["monitor", "service", "network", "host"]
+"""
+Monitors for SimpleMonitor
+"""
+
+from .arlo import MonitorArloCamera
+from .compound import CompoundMonitor
+from .file import MonitorBackup
+from .hass import MonitorSensor
+from .host import (
+    MonitorApcupsd,
+    MonitorCommand,
+    MonitorDiskSpace,
+    MonitorFileStat,
+    MonitorLoadAvg,
+    MonitorMemory,
+    MonitorPkgAudit,
+    MonitorPortAudit,
+    MonitorSwap,
+    MonitorZap,
+)
+from .network import MonitorDNS, MonitorHost, MonitorHTTP, MonitorTCP
+from .ring import MonitorRingDoorbell
+from .service import (
+    MonitorEximQueue,
+    MonitorProcess,
+    MonitorRC,
+    MonitorService,
+    MonitorSvc,
+    MonitorSystemdUnit,
+    MonitorUnixService,
+    MonitorWindowsDHCPScope,
+)
