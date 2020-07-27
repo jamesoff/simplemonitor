5803b5cf42fed832ee7b29fd0b80527a422d137e
James Seward <james@jamesoff.net>
2020-07-27 15:13:25 +0100

    Handle exceptions in unifi monitor

--- simplemonitor/Monitors/unifi.py
+++ simplemonitor/Monitors/unifi.py
@@ -5,6 +5,7 @@ from .monitor import Monitor, register
 
 try:
     from paramiko.client import SSHClient, RejectPolicy
+    from paramiko.ssh_exception import SSHException
 
     ssh2_available = True
 except ImportError:
@@ -40,38 +41,44 @@ class MonitorUnifiFailover(Monitor):
         if not ssh2_available:
             return self.record_fail("ssh2_python library is not installed")
 
-        with SSHClient() as client:
-            client.set_missing_host_key_policy(RejectPolicy)
-            client.connect(
-                hostname=self._router_address,
-                username=self._username,
-                password=self._password,
-                key_filename=self._ssh_key,
-            )
-            _, stdout, _ = client.exec_command("sudo /usr/sbin/ubnt-hal wlbGetStatus")
-            data = {}  # type: Dict[str, Dict[str, str]]
-            data_block = {}  # type: Dict[str, str]
-            interface = ""
-            for _line in stdout.readlines():
-                line = _line.strip()
-                matches = re.match(r"interface +: (\w+)", line)
-                if matches:
-                    if interface != "" and len(data_block) > 0:
-                        data[interface] = data_block
+        try:
+            with SSHClient() as client:
+                client.set_missing_host_key_policy(RejectPolicy)
+                client.connect(
+                    hostname=self._router_address,
+                    username=self._username,
+                    password=self._password,
+                    key_filename=self._ssh_key,
+                )
+                _, stdout, _ = client.exec_command(
+                    "sudo /usr/sbin/ubnt-hal wlbGetStatus"
+                )
+                data = {}  # type: Dict[str, Dict[str, str]]
+                data_block = {}  # type: Dict[str, str]
+                interface = ""
+                for _line in stdout.readlines():
+                    line = _line.strip()
+                    matches = re.match(r"interface +: (\w+)", line)
+                    if matches:
+                        if interface != "" and len(data_block) > 0:
+                            data[interface] = data_block
+                            data_block = {}
+                        interface = matches.group(1)
                         data_block = {}
-                    interface = matches.group(1)
-                    data_block = {}
-                    continue
-                matches = re.match(r"carrier +: (\w+)", line)
-                if matches:
-                    data_block["carrier"] = matches.group(1)
-                    continue
-                matches = re.match(r"status +: (\w+)", line)
-                if matches:
-                    data_block["status"] = matches.group(1)
-                    continue
-            if interface != "" and len(data_block) > 0:
-                data[interface] = data_block
+                        continue
+                    matches = re.match(r"carrier +: (\w+)", line)
+                    if matches:
+                        data_block["carrier"] = matches.group(1)
+                        continue
+                    matches = re.match(r"status +: (\w+)", line)
+                    if matches:
+                        data_block["status"] = matches.group(1)
+                        continue
+                if interface != "" and len(data_block) > 0:
+                    data[interface] = data_block
+        except SSHException as error:
+            self.monitor_logger.exception("Failed to ssh to USG")
+            return self.record_fail("Failed to ssh to USG: {}".format(error))
         if self._check_interface not in data:
             self.monitor_logger.debug("processed data was %s", data)
             return self.record_fail(
