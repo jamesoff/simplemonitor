6eae7c88163c9fa2698f2a434f57efacde9ecde3
James Seward <james@jamesoff.net>
2022-01-08 14:11:48 +0000

    Lint and fix uninitialised variable name

--- simplemonitor/Alerters/nextcloud_notification.py
+++ simplemonitor/Alerters/nextcloud_notification.py
@@ -2,10 +2,13 @@
 SimpleMonitor alerts via Nextcloud Notifications
 """
 
-import requests
 from typing import cast
+
+import requests
+
 from ..Monitors.monitor import Monitor
 from .alerter import Alerter, AlertLength, AlertType, register
+
 
 @register
 class NextcloudNotificationAlerter(Alerter):
@@ -28,11 +31,14 @@ class NextcloudNotificationAlerter(Alerter):
 
         self.support_catchup = True
 
-    def send_nextcloud_notification(self,shortMessage: str, longMessage: str) -> None:
+    def send_nextcloud_notification(self, shortMessage: str, longMessage: str) -> None:
         uri = "ocs/v2.php/apps/notifications/api/v2/admin_notifications"
         requests.post(
-            f"https://{self.nextcloud_user}:{self.nextcloud_token}@{self.nextcloud_server}/{uri}/{self.nextcloud_receiver}",
-            headers ={'OCS-APIREQUEST': 'true'},
+            (
+                f"https://{self.nextcloud_user}:{self.nextcloud_token}"
+                f"@{self.nextcloud_server}/{uri}/{self.nextcloud_receiver}"
+            ),
+            headers={"OCS-APIREQUEST": "true"},
             data={
                 "shortMessage": shortMessage,
                 "longMessage": longMessage,
@@ -46,16 +52,20 @@ class NextcloudNotificationAlerter(Alerter):
 
         if alert_type == AlertType.NONE:
             return
-        shortMessage = self.build_message(AlertLength.NOTIFICATION, alert_type, monitor)
-        longMessage = self.build_message(AlertLength.FULL, alert_type, monitor)
+        short_message = self.build_message(
+            AlertLength.NOTIFICATION, alert_type, monitor
+        )
+        long_message = self.build_message(AlertLength.FULL, alert_type, monitor)
 
         if not self._dry_run:
             try:
-                self.send_nextcloud_notification(shortMessage, longMessage)
+                self.send_nextcloud_notification(short_message, long_message)
             except Exception:
                 self.alerter_logger.exception("Couldn't send push notification")
         else:
-            self.alerter_logger.info("dry_run: would send push notification: %s", body)
+            self.alerter_logger.info(
+                "dry_run: would send push notification: %s", long_message
+            )
 
     def _describe_action(self) -> str:
         return "posting a Nextcloud Notification"
