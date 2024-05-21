1e9b02f414966927a4eca4c2a6617c6be4c72151
wsw70 <1345886+wsw70@users.noreply.github.com>
2024-05-21 12:59:37 +0200

    formatting by black

--- simplemonitor/Monitors/remote_ssh.py
+++ simplemonitor/Monitors/remote_ssh.py
@@ -36,34 +36,64 @@ class MonitorRemoteSSH(Monitor):
     def __init__(self, name: str, config_options: dict) -> None:
         super().__init__(name, config_options)
         # description
-        self.description = cast(str, self.get_config_option("description", required=False))  # maybe define default here instead of a try: ?
-        self.success_message = cast(str, self.get_config_option("success_message", required=False, default="it worked"))
+        self.description = cast(
+            str, self.get_config_option("description", required=False)
+        )  # maybe define default here instead of a try: ?
+        self.success_message = cast(
+            str,
+            self.get_config_option(
+                "success_message", required=False, default="it worked"
+            ),
+        )
         # ssh configuration
         self.command = cast(str, self.get_config_option("command", required=True))
-        self.ssh_private_key_path = cast(str, self.get_config_option("ssh_private_key_path", required=True))
-        self.ssh_username = cast(str, self.get_config_option("ssh_username", required=True))
-        self.target_hostname = cast(str, self.get_config_option("target_hostname", required=True))
-        self.target_port = cast(int, self.get_config_option("target_port", required=False, default="22"))
+        self.ssh_private_key_path = cast(
+            str, self.get_config_option("ssh_private_key_path", required=True)
+        )
+        self.ssh_username = cast(
+            str, self.get_config_option("ssh_username", required=True)
+        )
+        self.target_hostname = cast(
+            str, self.get_config_option("target_hostname", required=True)
+        )
+        self.target_port = cast(
+            int, self.get_config_option("target_port", required=False, default="22")
+        )
         # operator logic
         self.operator = cast(
             str,
             self.get_config_option(
                 "operator",
                 required=True,
-                allowed_values=[Operator.EQUALS.value, Operator.NOT_EQUALS.value, Operator.LESS_THAN.value, Operator.GREATER_THAN.value],
+                allowed_values=[
+                    Operator.EQUALS.value,
+                    Operator.NOT_EQUALS.value,
+                    Operator.LESS_THAN.value,
+                    Operator.GREATER_THAN.value,
+                ],
             ),
         )
-        self.regex = re.compile(cast(str, self.get_config_option("regex", required=True)))
+        self.regex = re.compile(
+            cast(str, self.get_config_option("regex", required=True))
+        )
         # values to compare, cast to the expected type
         self.result_type = cast(
             str,
-            self.get_config_option("result_type", required=True, allowed_values=[OperatorType.INTEGER.value, OperatorType.STRING.value]),
+            self.get_config_option(
+                "result_type",
+                required=True,
+                allowed_values=[OperatorType.INTEGER.value, OperatorType.STRING.value],
+            ),
         )
         match self.result_type:
             case OperatorType.INTEGER.value:
-                self.target_value = cast(int, self.get_config_option("target_value", required=True))
+                self.target_value = cast(
+                    int, self.get_config_option("target_value", required=True)
+                )
             case OperatorType.STRING.value:
-                self.target_value = cast(str, self.get_config_option("target_value", required=True))
+                self.target_value = cast(
+                    str, self.get_config_option("target_value", required=True)
+                )
 
     def run_test(self) -> bool:
         # run remote command
@@ -90,7 +120,6 @@ class MonitorRemoteSSH(Monitor):
                 )
             else:
                 _, stdout, _ = client.exec_command(self.command)
-
             # extract and cast the actual value
             try:
                 # let's hope for the best
@@ -106,7 +135,9 @@ class MonitorRemoteSSH(Monitor):
             case OperatorType.STRING.value:
                 actual_value = cast(str, actual_value)
 
-        # assess the comparison logic. str and int can be checked for equality, only int can be checked for greater/less than
+        # assess the comparison logic.
+        # str and int can be checked for equality,
+        # but only int can be checked for greater/less than
         test_succeeded = False  # better be pessimistic
         match self.operator:
             case Operator.EQUALS.value:
@@ -117,12 +148,16 @@ class MonitorRemoteSSH(Monitor):
                 test_succeeded = actual_value > self.target_value
             case Operator.LESS_THAN.value:
                 test_succeeded = actual_value < self.target_value
-        if self.result_type == OperatorType.STRING.value and (self.operator in [Operator.GREATER_THAN.value, Operator.LESS_THAN.value]):
+        if self.result_type == OperatorType.STRING.value and (
+            self.operator in [Operator.GREATER_THAN.value, Operator.LESS_THAN.value]
+        ):
             logger.warning(f"strings compared with '{self.operator}'")
         if test_succeeded:
             return self.record_success(self.success_message.format(actual_value))
         else:
-            return self.record_fail(f"actual value: {actual_value} | operator: {self.operator} | target value: {self.target_value}")
+            return self.record_fail(
+                f"actual value: {actual_value} | operator: {self.operator} | target value: {self.target_value}"
+            )
 
     def get_params(self) -> Tuple:
         return (
