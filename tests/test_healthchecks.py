c879c5836716307848de2e8745f4b5a8a10461e7
filak <filak@users.noreply.github.com>
2024-10-13 15:17:55 +0200

    Update test_healthchecks.py

--- tests/test_healthchecks.py
+++ tests/test_healthchecks.py
@@ -15,6 +15,7 @@ class TestHealthchecksAlerter(unittest.TestCase):
 
         # Instantiate the Alerter
         config_options = {"token": "123456789"}
+
         alerter = HealthchecksAlerter(config_options)
 
         # Test the send_notification method
@@ -77,6 +78,7 @@ class TestHealthchecksAlerter(unittest.TestCase):
 
         # Instantiate the Alerter
         config_options = {"token": "123456789"}
+
         alerter = HealthchecksAlerter(config_options)
 
         # Test the send_notification method
@@ -90,6 +92,38 @@ class TestHealthchecksAlerter(unittest.TestCase):
 
         # Check that requests.post was called with the correct URL
         expected_url = "https://hc-ping.com/123456789/test-slug/fail"
+        mock_post.assert_called_once_with(
+            expected_url,
+            data="Test Body",
+            headers={"User-Agent": "SimpleMonitor"},
+            timeout=5,
+        )
+
+    @patch("requests.post")
+    def test_send_notification_failed_create(self, mock_post):
+        """Test sending a notification successfully."""
+        # Mock the response from requests
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_post.return_value = mock_response
+
+        # Instantiate the Alerter
+        config_options = {"token": "123456789"}
+        config_options["create"] = True
+
+        alerter = HealthchecksAlerter(config_options)
+
+        # Test the send_notification method
+        alerter.send_notification(
+            subject="Test Subject",
+            body="Test Body",
+            alert_type="failed",
+            name="Test Monitor",
+            slug="test-slug",
+        )
+
+        # Check that requests.post was called with the correct URL
+        expected_url = "https://hc-ping.com/123456789/test-slug/fail?create=1"
         mock_post.assert_called_once_with(
             expected_url,
             data="Test Body",
